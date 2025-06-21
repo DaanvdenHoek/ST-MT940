9c992451c00c19719db9285fd04706a1d2c2a7d3
DaanvdenHoek <92542687+DaanvdenHoek@users.noreply.github.com>
2025-06-21 21:00:23 +0200

    add app.py and format

--- src/main.py
+++ src/main.py
@@ -1,52 +1,54 @@
 import streamlit as st
 from src.parser import parse_mt940, df_to_xlsx_bytes
-from src.options import option_map 
+from src.options import option_map
 
 OUTPUT_FILE_NAME = "SwiftTransactions.xlsx"
 
-def reset_converted_file() -> None:
-    st.session_state.converted_file = None
 
-if "converted_file" not in st.session_state:
-    reset_converted_file()
+def run():
+    def reset_converted_file() -> None:
+        st.session_state.converted_file = None
 
-st.title("Swift MT904 Parser")
+    if "converted_file" not in st.session_state:
+        reset_converted_file()
 
-files = st.file_uploader(
-    label="Select file(s) to parse",
-    accept_multiple_files=True,
-    type="swi",
-    on_change=reset_converted_file
-)
+    st.title("Swift MT904 Parser")
 
-processing_label = st.radio(
-    "Choose output format:",
-    options=list(option_map.keys()),
-    help="""
-    - **One Excel file with multiple worksheets**: Each uploaded file becomes its own tab (worksheet) in one Excel file.  
-    - **One Excel file with all data in a single worksheet**: All data is merged into one sheet.  
-    - **Separate Excel files per source file**: You’ll get multiple Excel files, one for each input file.
-    """,
-    on_change=reset_converted_file
-)
+    files = st.file_uploader(
+        label="Select file(s) to parse",
+        accept_multiple_files=True,
+        type="swi",
+        on_change=reset_converted_file,
+    )
 
-if files:
-    if st.button("Parse"):
-        file_names = [file.name.rsplit(".")[0] for file in files]
-        if len(set(file_names)) != len(file_names):
-            st.error("Duplicate filenames found")
-        else:
-            dfs = [parse_mt940(file) for file in files]
-            processing_option = option_map[processing_label]
-            data = df_to_xlsx_bytes(dfs, file_names, processing_option)
-            st.session_state.converted_file = data
-            st.success("File parsed and ready for download.")
+    processing_label = st.radio(
+        "Choose output format:",
+        options=list(option_map.keys()),
+        help="""
+        - **One Excel file with multiple worksheets**: Each uploaded file becomes its own tab (worksheet) in one Excel file.  
+        - **One Excel file with all data in a single worksheet**: All data is merged into one sheet.  
+        - **Separate Excel files per source file**: You’ll get multiple Excel files, one for each input file.
+        """,
+        on_change=reset_converted_file,
+    )
 
-    if st.session_state.converted_file:
-        st.download_button(
-            label="Download",
-            data=st.session_state.converted_file,
-            file_name=OUTPUT_FILE_NAME,
-            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-            on_click=reset_converted_file
-        )
+    if files:
+        if st.button("Parse"):
+            file_names = [file.name.rsplit(".")[0] for file in files]
+            if len(set(file_names)) != len(file_names):
+                st.error("Duplicate filenames found")
+            else:
+                dfs = [parse_mt940(file) for file in files]
+                processing_option = option_map[processing_label]
+                data = df_to_xlsx_bytes(dfs, file_names, processing_option)
+                st.session_state.converted_file = data
+                st.success("File parsed and ready for download.")
+
+        if st.session_state.converted_file:
+            st.download_button(
+                label="Download",
+                data=st.session_state.converted_file,
+                file_name=OUTPUT_FILE_NAME,
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                on_click=reset_converted_file,
+            )
--- src/options.py
+++ src/options.py
@@ -1,10 +1,12 @@
 from enum import Enum
 
+
 class OutputOption(str, Enum):
     MULTI_SHEET = "multi_sheet"
     SINGLE_SHEET = "single_sheet"
 
+
 option_map = {
     "Multiple sheets": OutputOption.MULTI_SHEET,
-    "Single sheet": OutputOption.SINGLE_SHEET
+    "Single sheet": OutputOption.SINGLE_SHEET,
 }
